d490293ff997bb91b9e199cc430be6bd29575d80
Danilo <danilo.pa88@gmail.com>
2026-04-18 16:21:11 -0300

    fix: nome do campo errado

--- dbt/models/silver/fct_despesas.sql
+++ dbt/models/silver/fct_despesas.sql
@@ -5,14 +5,14 @@ WITH stg AS (
 )
 
 SELECT
-    {{ dbt_utils.generate_surrogate_key(['deputado_id', 'data_documento', 'numero_documento', 'valor_documento']) }} AS despesa_key,
+    {{ dbt_utils.generate_surrogate_key(['deputado_id', 'data_documento', 'numero_documento', 'valor_bruto']) }} AS despesa_key,
     deputado_id,
     ano,
     mes,
     tipo_despesa,
     data_documento,
     numero_documento,
-    valor_documento,
+    valor_bruto,
     nome_fornecedor,
     cnpj_cpf_fornecedor,
     url_documento,
