edf4492726497cc8e2187062f9a078039601019f
Danilo <danilo.pa88@gmail.com>
2026-05-04 22:12:19 -0300

    Deduplicando

--- dbt/models/silver/fct_despesas.sql
+++ dbt/models/silver/fct_despesas.sql
@@ -2,9 +2,10 @@
 -- Camada Silver: Fato de despesas limpa e tipada
 WITH stg AS (
     SELECT * FROM {{ ref('stg_despesas') }}
-)
+);
 
-SELECT
+WITH clean_stg AS(
+SELECT 
     {{ dbt_utils.generate_surrogate_key(['id', 'deputy_id', 'expense_date', 'document_number', 'gross_amount']) }} AS expense_key,
     id,
     deputy_id,
@@ -21,3 +22,8 @@ SELECT
     modified_at
 FROM stg
 WHERE gross_amount > 0
+)
+
+SELECT *, ROW_NUMBER() OVER(PARTITION BY expense_key ORDER BY processed_at DESC) as rn
+FROM clean_stg
+WHERE rn = 1
