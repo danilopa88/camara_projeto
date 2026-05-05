32ad379c11a0f78e6459b90989b7648e0cada73c
Danilo <danilo.pa88@gmail.com>
2026-05-04 22:16:22 -0300

    feat: add silver layer fct_despesas model with surrogate key generation and deduplication logic

--- dbt/models/silver/fct_despesas.sql
+++ dbt/models/silver/fct_despesas.sql
@@ -2,9 +2,9 @@
 -- Camada Silver: Fato de despesas limpa e tipada
 WITH stg AS (
     SELECT * FROM {{ ref('stg_despesas') }}
-);
+)
 
-WITH clean_stg AS(
+clean_stg AS(
 SELECT 
     {{ dbt_utils.generate_surrogate_key(['id', 'deputy_id', 'expense_date', 'document_number', 'gross_amount']) }} AS expense_key,
     id,
@@ -22,7 +22,7 @@ SELECT
     modified_at
 FROM stg
 WHERE gross_amount > 0
-)
+);
 
 SELECT *, ROW_NUMBER() OVER(PARTITION BY expense_key ORDER BY processed_at DESC) as rn
 FROM clean_stg
